f3b8b30b7f37464b1d47f209b8d60a9e77c34399
ivanbakrac <48951541+ivanbakrac@users.noreply.github.com>
2020-01-25 14:06:45 -0500

    Update ib.py

--- ib.py
+++ ib.py
@@ -1,8 +1,9 @@
 import numpy as np
 import matplotlib.pylab as plt
-x = np.linspace(0, 2*np.pi)
+
+x = np.linspace(0, 2 * np.pi)
 y = np.cos(x)
 z = np.sin(x)
-plt.plot(x,y)
-plt.plot(x,z)
+plt.plot(x, y)
+plt.plot(x, z)
 plt.show()
