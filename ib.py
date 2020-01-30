87a040c989cb8550dce314665139bb254a6bf3c7
ivanbakrac <48951541+ivanbakrac@users.noreply.github.com>
2020-01-30 08:39:03 -0500

    Add files via upload

--- ib.py
+++ ib.py
@@ -1,1 +1,15 @@
-print("hell0")
+import numpy as np
+import matplotlib.pylab as plt
+
+
+def mysin():
+    x = np.linspace(0, 2 * np.pi, num=150)
+    y = np.cos(x)
+    z = np.sin(x)
+    plt.plot(x, y)
+    plt.plot(x, z)
+    plt.show()
+
+
+if __name__ == "__main__":
+    mysin()
